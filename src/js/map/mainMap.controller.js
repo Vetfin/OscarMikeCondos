e8961876cc7cf2936fb60f38e0fce974cbf0d961
Stella Ma <sm873x@gmail.com>
2016-08-23 14:57:40 -0400

    create main map controller for home view

---
+++ src/js/map/mainMap.controller.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    // angular.module('vacondos')
+    //     .controller('mainMap', MainMapController);
+    //
+    // function MainMapController() {
+    //     this.map = function initMap(latNum, lngNum) {
+    //         new google.maps.Map($('#main-map'), {
+    //             center: {lat: latNum, lng: lngNum
+    //             },
+    //             zoom: 8
+    //         });
+    //     };
+    // }
+})();
