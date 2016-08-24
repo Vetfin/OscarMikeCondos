58b438ad3486790a07d4d623fd4240884257ad31
Stella Ma <sm873x@gmail.com>
2016-08-24 11:42:27 -0400

    Implemented google map example with india cities data.

--- src/js/map/mainMap.controller.js
+++ src/js/map/mainMap.controller.js
@@ -1,16 +1,55 @@
 (function() {
     'use strict';
 
-    // angular.module('vacondos')
-    //     .controller('mainMap', MainMapController);
-    //
-    // function MainMapController() {
-    //     this.map = function initMap(latNum, lngNum) {
-    //         new google.maps.Map($('#main-map'), {
-    //             center: {lat: latNum, lng: lngNum
-    //             },
-    //             zoom: 8
-    //         });
-    //     };
-    // }
+    angular.module('vacondos')
+        .controller('MainMapController', MainMapController);
+
+    MainMapController.$inject = ['maps'];
+
+    function MainMapController(maps) {
+        var that = this;
+
+        this.cities = maps.getExample();
+
+        var mapOptions = {
+          zoom: 4,
+          center: new google.maps.LatLng(25,80),
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+        };
+
+        this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        this.markers = [];
+
+        var infoWindow = new google.maps.InfoWindow();
+
+        var createMarker = function (info){
+            var marker = new google.maps.Marker({
+                map: that.map,
+                position: new google.maps.LatLng(info.lat, info.long),
+                title: info.city
+            });
+            marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+
+            google.maps.event.addListener(marker, 'click', function(){
+                infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                infoWindow.open(that.map, marker);
+          });
+
+          that.markers.push(marker);
+
+        };
+
+        var i;
+        for (i = 0; i < this.cities.length; i++){
+          createMarker(this.cities[i]);
+        }
+
+        this.openInfoWindow = function(e, selectedMarker){
+            e.preventDefault();
+            google.maps.event.trigger(selectedMarker, 'click');
+        };
+
+    }
+
 })();
