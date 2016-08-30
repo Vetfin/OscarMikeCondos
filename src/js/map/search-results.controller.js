228e8d56c22836f713dd9ddc031af6ab63b03330
Stella Ma <sm873x@gmail.com>
2016-08-30 16:38:07 -0400

    Added function to only get for sale condos and to get all va approved condo buildings

--- src/js/map/search-results.controller.js
+++ src/js/map/search-results.controller.js
@@ -10,24 +10,44 @@
         var that = this;
 
 
-        this.searchZip = null;
+        this.searchParams = {
+            min_price: 0,
+            max_price: null,
+            min_bed: 0,
+            max_bed: null,
+            min_bath: 0,
+            max_bath: null,
+            parking: null,
+            zip: null
+        };
 
         this.condos = [];
-        
-        condos
-            .getAllCondos()
-            .then(function(allCondos) {
-                that.condos = allCondos;
-                // that.makeCondoMarkers(allCondos);
-                console.log('condos', that.condos);
-            });
+
+        this.getForSaleCondos = function getForSaleCondos() {
+            condos
+                .getCondosForSale(that.searchParams)
+                .then(function(condosForSale) {
+                    that.condos = condosForSale;
+                    console.log('returned condos for sale', that.condos);
+                });
+        };
+
+        this.getAllBuildings = function getAllBuildings() {
+            condos
+                .getAllCondos()
+                .then(function(allApprovedBuildings) {
+                    that.condos = allApprovedBuildings;
+                    // that.makeCondoMarkers(allCondos);
+                    console.log('returned all buildings', that.condos);
+                });
+        };
 
         /** DC LatLng: 38.907192, -77.036871 **/
 
-        // this.openInfoWindow = function(e, selectedMarker){
-        //     e.preventDefault();
-        //     google.maps.event.trigger(selectedMarker, 'click');
-        // };
+        this.openInfoWindow = function(e, selectedMarker){
+            e.preventDefault();
+            google.maps.event.trigger(selectedMarker, 'click');
+        };
 
     }
 
