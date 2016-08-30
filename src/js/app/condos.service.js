e6663bae1387a5b0d3a8d3a3b7c6a4303237d429
Stella Ma <sm873x@gmail.com>
2016-08-30 16:29:01 -0400

    Added data params to be sent with get condos for sale http call.

--- src/js/app/condos.service.js
+++ src/js/app/condos.service.js
@@ -14,16 +14,28 @@
 
         function getAllCondos() {
             return $http({
-                url: 'https://arcane-spire-51321.herokuapp.com/condos.json'
+                url: 'https://arcane-spire-51321.herokuapp.com/condos.json',
+                method: 'get'
             })
             .then(function(response) {
                 return response.data;
             });
         }
 
-        function getCondosForSale() {
+        function getCondosForSale(paramObj) {
             return $http({
-                url: 'https://arcane-spire-51321.herokuapp.com/condos.json'
+                url: 'https://arcane-spire-51321.herokuapp.com/condos.json',
+                method: 'get',
+                dataType: 'json',
+                data: {
+                    min_price: paramObj.min_price,
+                    max_price: paramObj.max_price,
+                    min_bed: paramObj.min_bed,
+                    max_bed: paramObj.max_bed,
+                    min_bath: paramObj.min_bath,
+                    max_bath: paramObj.max_bath,
+                    zip: paramObj.zip
+                }
             })
             .then(function(response) {
                 return response.data;
