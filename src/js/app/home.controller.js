a012f891f19048f3e2cbb78f931c687e10b989bd
Stella Ma <sm873x@gmail.com>
2016-08-31 12:49:37 -0400

    Deleted legacy functions from previous attempt on home controller

--- src/js/app/home.controller.js
+++ src/js/app/home.controller.js
@@ -19,26 +19,6 @@
             zip: null
         };
 
-        // this.results = [];
-        //
-        // this.getCondos = function getCondos() {
-        //     condos
-        //         .getAllCondos()
-        //         .then(function(condosForSale) {
-        //             that.results = condosForSale;
-        //             console.log('returned condos for sale', that.results);
-        //         });
-        // };
-        //
-        // this.getAllApproved = function getAllApprovedAddresses() {
-        //     condos
-        //         .getAllAddresses()
-        //         .then(function(buildings) {
-        //             that.results = buildings;
-        //             console.log('returned all buildings', that.results);
-        //         });
-        // };
-
         this.goToResults = function goToSearchResults() {
             condos.getAllCondos()
                 .then(function() {
