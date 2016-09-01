ac8b1d4bc1a3276adcb91ad069cf913bd46d2dcf
Stella Ma <sm873x@gmail.com>
2016-09-01 07:44:47 -0400

    Added sanity test to condos service test for all current functions. Added test for allAddresses fn.

--- test/specs/condos.service.spec.js
+++ test/specs/condos.service.spec.js
@@ -14,14 +14,54 @@
 
             $httpBackend
                 .whenGET('https://arcane-spire-51321.herokuapp.com/addresses.json')
-                .respond([
-                    { address: '123 Penn St Washington DC', lat: 22, lng: 77},
-                    { address: '321 Lime St Washington DC', lat: -30, lng: 77}
-                ]);
+                .respond([{
+                    address: '123 Penn St Washington DC',
+                    latitude: 22,
+                    longitude: 77
+                }]);
+
+            $httpBackend
+                .whenGET('/js/templates/home.template.html')
+                .respond('<h1>Mock Home Template</h1>');
         }));
 
         test('condos service functions exist', function() {
             assert.isFunction(condos.getAllAddresses, 'getAllAddresses fn exists');
+            assert.isFunction(condos.getAllCondos, 'getAllCondos fn exists');
+            assert.isFunction(condos.getSearchResults, 'getSearchResults fn exists');
+        });
+
+        test('getAllAddresses successfully returns all addresses', function(done) {
+            var result = condos.getAllAddresses();
+
+            assert.isObject(result, 'getAllAddresses returns an object');
+            assert.isFunction(result.then, 'result has a then method');
+            assert.isFunction(result.catch, 'result has a catch method');
+
+            result
+                .then(function(allAddresses) {
+                    console.log(allAddresses);
+                    assert
+                        .isArray(allAddresses.data, 'the data property of allAddresses is an array');
+                    assert
+                        .strictEqual(
+                            allAddresses.data[0].address,
+                            '123 Penn St Washington DC',
+                            'allAddresses has address'
+                        );
+                    assert.strictEqual(allAddresses.data[0].latitude, 22, 'allAddresses has latitude');
+                    assert.isNumber(allAddresses.data[0].latitude);
+                    assert.strictEqual(allAddresses.data[0].longitude, 77, 'allAddresses has longitude');
+                    assert.isNumber(allAddresses.data[0].longitude);
+                    done();
+                })
+                .catch(function(err) {
+                    console.log(err);
+                    assert.isFail('should not be in catch for getAllAddresses');
+                    done();
+                });
+
+            $httpBackend.flush();
         });
 
     });
