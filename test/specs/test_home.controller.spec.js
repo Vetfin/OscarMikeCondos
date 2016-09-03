7eb1e778f4e7a56b8dc2a12243ffc4619f686034
Stella Ma <sm873x@gmail.com>
2016-09-03 19:08:40 -0400

    Added test for correctproperties in searchParams object

--- test/specs/test_home.controller.spec.js
+++ test/specs/test_home.controller.spec.js
@@ -20,17 +20,25 @@
                 .whenGET('/js/templates/home.template.html')
                 .respond('<h1>Mock Home Template</h1>');
 
+            $httpBackend
+                .whenGET('/js/templates/search-results.template.html')
+                .respond('<h1>Mock Search Results Template</h1>');
+
             mockCondosService.getAllBuildings = function() {
+                mockCondosService.getAllBuildings.called++;
                 var def = $q.defer();
                 def.resolve([]);
                 return def.promise;
             };
+            mockCondosService.getAllBuildings.called = 0;
 
             mockCondosService.getAllCondos = function() {
+                mockCondosService.getAllCondos.called++;
                 var def = $q.defer();
                 def.resolve([]);
                 return def.promise;
             };
+            mockCondosService.getAllCondos.called = 0;
 
             homeController = $controller('HomeController');
         }));
@@ -47,5 +55,44 @@
             assert.isFunction(homeController.goToAll, 'controller has method for going to results for all buildings');
             $rootScope.$digest();
         });
+
+        test('goToResults will search for condos', function() {
+            homeController.goToResults();
+
+            assert
+                .strictEqual(
+                        mockCondosService.getAllCondos.called,
+                        1,
+                        'the condos service getAllCondos method was called'
+                );
+
+            $rootScope.$digest();
+        });
+
+        test('goToAll will search for all buildings', function() {
+            homeController.goToAll();
+
+            assert
+                .strictEqual(
+                        mockCondosService.getAllBuildings.called,
+                        1,
+                        'the condos service getAllBuildings method was called'
+                );
+
+            $rootScope.$digest();
+        });
+
+        test('searchParams object has correct properties', function() {
+            var params = homeController.searchParams;
+
+            assert.property(params, 'min_price', 'searchParams has min_price');
+            assert.property(params, 'max_price', 'searchParams has max_price');
+            assert.property(params, 'min_bed', 'searchParams has min_bed');
+            assert.property(params, 'max_bed', 'searchParams has max_bed');
+            assert.property(params, 'min_bath', 'searchParams has min_bath');
+            assert.property(params, 'max_bath', 'searchParams has max_bath');
+            assert.property(params, 'zip', 'searchParams has zip');
+        });
+
     });
 })();
