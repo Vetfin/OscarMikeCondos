9dc084f81bd899b928dda45e433a0d01e5abd9d2
Stella Ma <sm873x@gmail.com>
2016-09-04 01:41:59 -0400

    Added form for creating an account and bound to controller. User service handles the data post.

--- src/js/accounts/create-account.controller.js
+++ src/js/accounts/create-account.controller.js
@@ -4,8 +4,28 @@
     angular.module('vacondos')
         .controller('CreateAccountController', CreateAccountController);
 
-    function CreateAccountController() {
-        console.log('create account');
+    CreateAccountController.$inject = ['user'];
+
+    function CreateAccountController(user) {
+        console.log('in create account');
+
+        var that = this;
+
+        this.userInfo = {};
+
+        this.createAccount = function createAccount() {
+            console.log('start creating new account');
+
+            user.createUser(
+                that.userInfo.nameFirst,
+                that.userInfo.nameLast,
+                that.userInfo.email,
+                that.userInfo.password
+            );
+            //TODO add then and catch after $http is added
+            //TODO erase inputs after successfully signing up
+            //TODO do not show create account after this
+        };
     }
-    
+
 })();
