415b26649fa8a5e0a6f28dd1be6d3b867d3a3303
Stella Ma <sm873x@gmail.com>
2016-09-06 12:21:22 -0400

    Added create account functionality with post to /users. Captures current user info and logged status.

--- src/js/accounts/user.service.js
+++ src/js/accounts/user.service.js
@@ -4,17 +4,35 @@
     angular.module('vacondos')
         .factory('user', UserService);
 
-        // UserService.$inject = ['http']; TODO add $http call when ready
+    UserService.$inject = ['$http'];
 
-    function UserService() {
+    function UserService($http) {
+        var currentUser;
+
         return {
             createUser: createUser
         };
 
         //TODO Add $http post to create user account
-        function createUser(nameFirst, nameLast, email, password) {
-            console.log(nameFirst, nameLast, email, password);
-            //TODO instantiate var currentUser above and assign the data that comes back
+        function createUser(nameFirst, nameLast, userEmail, userPassword) {
+            console.log(nameFirst, nameLast, userEmail, userPassword);
+            //TODO Add $q.reject for if statement
+            return $http({
+                url: 'https://tiy-blog-api.herokuapp.com/api/users',
+                method: 'post',
+                dataType: 'json',
+                params: {
+                    first_name: nameFirst,
+                    last_name: nameLast,
+                    email: userEmail
+                    // password_digest: userPassword
+                }
+            })
+            .then(function functionName(user) {
+                currentUser = user.data;
+                return user;
+            });
+
         }
     }
 
