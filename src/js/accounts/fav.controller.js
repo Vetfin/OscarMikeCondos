3aa5f8109304c13bbf6746964e6bda657261e085
Stella Ma <sm873x@gmail.com>
2016-09-07 10:30:16 -0400

    Added logged in user info to fav controller in order to retrieve saved favorites

--- src/js/accounts/fav.controller.js
+++ src/js/accounts/fav.controller.js
@@ -4,7 +4,13 @@
     angular.module('vacondos')
         .controller('FavoritesController', FavoritesController);
 
-    function FavoritesController() {
-        console.log('in favorites ctrl');
+    FavoritesController.$inject = ['auth'];
+
+    function FavoritesController(auth) {
+        this.loggedInUser = auth.getLoggedInUser();
+
+        //TODO uncomment this to get favorites Array
+        //should be empty if user has not favorited anything yet
+        // this.favorites = this.loggedInUser.favorites;
     }
 })();
